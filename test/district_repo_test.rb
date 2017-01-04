532d38f5a6d10bee7406d779a6ab195a40e0bd5d
Andy Wooten <andrewdwooten@icloud.com>
2017-01-04 13:37:12 -0700

    Added require csv to district_repo_test

--- test/district_repo_test.rb
+++ test/district_repo_test.rb
@@ -1,14 +1,16 @@
 require_relative 'test_helper.rb'
-require '/fixtures/kindergarteners_fixture.csv'
+require './lib/district_repo.rb'
+require 'csv'
 
 class DistrictRepoTest < MiniTest::Test
 	attr_reader :dr
 
 	def setup
+	
 		@dr = DistrictRepository.new
 			dr.load_data({
   			:enrollment => {
-    			:kindergarten => "/fixtures/kindergarteners_fixture.csv"
+    			:kindergarten => "./test/fixtures/kindergarteners_fixture.csv"
 				}})
 	end
 
@@ -17,20 +19,24 @@ class DistrictRepoTest < MiniTest::Test
 		end
 
 		def test_district_repo_will_accept_data
+			
 			dr_contents = dr.contents
 			assert_equal Array, dr_contents
 			assert_equal District, dr_contents[0].class
 		end
 
 		def test_district_repo_can_find_district_by_name
+			skip
 			assert_equal District, dr.find_by_name("Academy").class #need to modify test to assess district attribute
 		end
 
 		def test_district_repo_returns_empty_array_if_no_partial_match
+			skip
 			assert_equal [], dr.find_all_matching('zzz')
 		end
 
 		def test_district_repo_returns_array_of_districts_if_partial_matches
+			skip
 			search_results = dr.find_all_matching('A')
 			assert_equal Array, search_results.class
 			assert_equal District, search_results[0].class
