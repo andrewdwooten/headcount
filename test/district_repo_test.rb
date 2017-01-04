ac535348bdce031ac43acc254a55fbbfd3f95321
Andy Wooten <andrewdwooten@icloud.com>
2017-01-04 15:39:07 -0700

    Added district_repo; all district and district_repo tests passing

--- test/district_repo_test.rb
+++ test/district_repo_test.rb
@@ -1,12 +1,11 @@
 require_relative 'test_helper.rb'
-require './lib/district_repo.rb'
+require_relative '../lib/district_repo.rb'
 require 'csv'
 
 class DistrictRepoTest < MiniTest::Test
 	attr_reader :dr
 
 	def setup
-	
 		@dr = DistrictRepository.new
 			dr.load_data({
   			:enrollment => {
@@ -19,25 +18,21 @@ class DistrictRepoTest < MiniTest::Test
 		end
 
 		def test_district_repo_will_accept_data
-			
 			dr_contents = dr.contents
-			assert_equal Array, dr_contents
+			assert_equal Array, dr_contents.class
 			assert_equal District, dr_contents[0].class
 		end
 
 		def test_district_repo_can_find_district_by_name
-			skip
-			assert_equal District, dr.find_by_name("Academy").class #need to modify test to assess district attribute
+			assert_equal District, dr.find_by_name("ACADEMY 20").class #need to modify test to assess district attribute
 		end
 
 		def test_district_repo_returns_empty_array_if_no_partial_match
-			skip
 			assert_equal [], dr.find_all_matching('zzz')
 		end
 
 		def test_district_repo_returns_array_of_districts_if_partial_matches
-			skip
-			search_results = dr.find_all_matching('A')
+			search_results = dr.find_all_matching('Aca')
 			assert_equal Array, search_results.class
 			assert_equal District, search_results[0].class
 		end
