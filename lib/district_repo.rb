ce3f3ee7e956628b60c798a21d121a48f3171139
Cody Borders <codyborders@gmail.com>
2017-01-08 17:24:26 -0700

    cleans up district_repo file

--- lib/district_repo.rb
+++ lib/district_repo.rb
@@ -3,12 +3,12 @@ require './lib/enrollment_repo.rb'
 require './lib/parser.rb'
 
 class DistrictRepository
-<<<<<<< HEAD
+
 	attr_reader :contents
-=======
+
 include Parser
-	attr_reader :contents 
->>>>>>> ba7528c8bcbb8844648a8856d2fa5da9d92c278d
+	attr_reader :contents
+
 	attr_accessor :enrollment
 
 	def initialize
@@ -17,15 +17,15 @@ include Parser
 	end
 
 	def load_data(load)
-<<<<<<< HEAD
+
 		data = CSV.read load.values[0].values[0],
 			headers: true, header_converters: :symbol
 		data.each {|row| contents << {:name => row[0]}}
 		contents.uniq!.collect! {|namer| District.new(namer, self)}
-=======
+
 		build_base(load).each do |base|
       contents << District.new(base, self) end
->>>>>>> ba7528c8bcbb8844648a8856d2fa5da9d92c278d
+
 		enrollment.load_data(load)
 	end
 
