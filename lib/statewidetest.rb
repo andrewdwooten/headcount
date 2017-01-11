21457e1f5dfb88c758e3207e2a574345cce546df
Andy Wooten <andrewdwooten@icloud.com>
2017-01-11 13:48:59 -0700

    Added methods for proficient by grade and proficient by race; added method to validate passed in races;we've got iteration 3 on the rungit status

--- lib/statewidetest.rb
+++ lib/statewidetest.rb
@@ -1,9 +1,11 @@
 class StatewideTest
-  attr_reader :name_stats, :name
+  attr_reader :name_stats, :name, :races, :subjects
 
   def initialize(name_stats)
     @name_stats = name_stats
     @name = name_stats[:name]
+    @races = [:asian, :black, :pacific_islander, :hispanic, :native_american, :two_or_more, :white]
+    @subjects = [:math, :reading, :writing]
   end
 
   def proficient_by_grade(grade)
@@ -15,4 +17,24 @@ class StatewideTest
     end
   end
 
+  def proficient_by_race_or_ethnicity(race)
+    if check_race?(race)
+      name_stats[race]
+    end
+  end
+
+  def proficient_for_subject_by_grade_in_year(subject, grade, year)
+  end
+
+  def check_race?(race)
+    if races.include?(race) != true
+      return 'raise UnknownRaceError'
+    else
+      true
+    end
+  end
+
+  def check_subject(subject)
+
+  end
 end
--- test/statewidetest_test.rb
+++ test/statewidetest_test.rb
@@ -40,9 +40,25 @@ class StatewideTestTest < MiniTest::Test
     2012=>{:math=>0.68197, :writing=>0.73839, :reading=>0.83352},                                                                                      
     2013=>{:math=>0.6613, :reading=>0.85286, :writing=>0.75069},                                                                                       
     2014=>{:math=>0.68496, :reading=>0.827, :writing=>0.74789}}
-		assert_equal output_a, sr.contents[0].proficient_by_grade(3)
-		assert_equal output_b, sr.contents[1].proficient_by_grade(8)
+		assert_equal output_a, sr.find_by_name('colorado').proficient_by_grade(3)
+		assert_equal output_b, sr.find_by_name('academy 20').proficient_by_grade(8)
 	end
 
+	def test_proficient_by_race_returns_hash_of_proficiencies
+		test1 = sr.find_by_name('Colorado').proficient_by_race_or_ethnicity(:asian)
+		test2 = sr.find_by_name('Academy 20').proficient_by_race_or_ethnicity(:black)
+		test3 = sr.find_by_name('Academy 20').proficient_by_race_or_ethnicity(:thai)
+		output_a = {2011=>{:math=>0.7094, :reading=>0.7482, :writing=>0.6569},                                                                                         
+    2012=>{:math=>0.7192, :reading=>0.7574, :writing=>0.6588},                                                                                         
+    2013=>{:math=>0.7323, :reading=>0.7692, :writing=>0.6821},                                                                                         
+    2014=>{:math=>0.7341, :reading=>0.7697, :writing=>0.6846}}
+		output_b = {2011=>{:math=>0.4246, :reading=>0.662, :writing=>0.5152},                                                                                          
+    2012=>{:math=>0.4248, :reading=>0.69469, :writing=>0.5044},                                                                                        
+    2013=>{:math=>0.4404, :reading=>0.66951, :writing=>0.4819},                                                                                        
+    2014=>{:math=>0.4205, :reading=>0.70387, :writing=>0.5194}}
+		assert_equal output_a, test1
+		assert_equal output_b, test2
+		assert_equal 'raise UnknownRaceError', test3
+	end
 
 	end
